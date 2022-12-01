766c01ebdcb500baed59cd977f7ce16c9ac4936c
Thomas Ebermann <thomas.ebermann@liip.ch>
2022-12-01 21:29:03 +0100

    update

--- remove_unwanted_news.js
+++ remove_unwanted_news.js
@@ -10,16 +10,30 @@
 // @match https://www.spiegel.de/*
 // @match https://www.reddit.com/*
 // @match https://www.facebook.com/*
+// @match https://www.nzz.ch/*
+// @match https://www.tagblatt.ch/*
+// @match https://www.tagesanzeiger.ch/*
+// @match https://zueriost.ch/*
+// @match https://www.vaterland.li/*
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js 
+// @require https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js
 // ==/UserScript==
 //https://gist.github.com/BrockA/2625891
 
+function truncate(str, n){
+  return (str.length > n) ? str.slice(0, n-1) + '&hellip;' : str;
+};
+
+var inform_about_removal = false;
 
 function recurseEl(father,element) {
   if(element.childElementCount === 0) {
-    search = /(stories)|(sponsored)|(kardashian)|(heizen)|(strom)|(macron)|(merz)|(elon)|(ukraine)|(selenskyj)|(liveticker)|(influencer)|(fifa)|(messi)|(infantino)|(corona)|(putin)|(bolsonaro)|(trump)|(trumps)|(arabischen)|(arabisch)|(jong)|(musk)|(promis)|(promi)|(katar)|(boateng)|(russland)|(russen)|(nati)|(weltmeister)/
-		if (element.innerText.toLowerCase().match(search)){
-			console.log("removing" + element.innerText)
+    search = /(william)|(stories)|(sponsored)|(kardashian)|(heizen)|(strom)|(macron)|(merz)|(elon)|(ukraine)|(selenskyj)|(liveticker)|(influencer)|(fifa)|(messi)|(infantino)|(corona)|(putin)|(bolsonaro)|(trump)|(trumps)|(arabischen)|(arabisch)|(jong)|(musk)|(promis)|(promi)|(katar)|(boateng)|(russland)|(russen)|(nati)|(weltmeister)/
+    if (element.innerText?.toLowerCase().match(search)){
+      console.log("removing" + element.innerText)
+      if (inform_about_removal){
+        $.notify("Removed: " + truncate(element.innerText,60),"success",{position:"right bottom"});
+      }
       element.textContent = '';
       father.style.display = "none";
     }
@@ -34,19 +48,34 @@ function updateHTML()
 {
   var href = window.location.host;
   if (href == "www.zeit.de"){
-		var selector =  ".zon-teaser-standard, .zon-teaser-wide, .zon-teaser-poster"
+    var selector =  ".zon-teaser-standard, .zon-teaser-wide, .zon-teaser-poster"
   }
   else if (href == "www.watson.ch"){
-  	var selector = '.region'
+    var selector = '.region'
   }
   else if (href == "www.spiegel.de"){
-  	var selector = "article"
+    var selector = "article"
+  }
+  else if (href == "www.tagblatt.ch"){
+    var selector = ".widget--1of3, .widget--2of3, .widget--3of3, .widget--1of2, .widget--1of2-top, .widget--2of2"
+  }
+  else if (href == "www.nzz.ch"){
+    var selector = ".widget--article , .teaser--1of3-news, .teaser--1of3-compact"
+  }
+  else if (href == "www.tagesanzeiger.ch"){
+    var selector = ".ArticleTeaser_colorbox__3dUIa, article"
+  }
+  else if (href == "zueriost.ch"){
+    var selector = ".col-xs-12"
+  }
+  else if (href == "www.vaterland.li"){
+    var selector = ".defaultteaser"
+  }
+  else if (href == "www.facebook.com"){
+    var selector = 'svg[viewBox="0 0 20 20"]' //search for the 3 dots in a post
   }
   else if (href == "www.reddit.com"){
     var selector = ".link"
-  }
-  else if (href == "www.facebook.com"){
-		var selector = 'svg[viewBox="0 0 20 20"]'
   }
   $(selector).each(function(i, obj) {
     try {
@@ -58,15 +87,14 @@ function updateHTML()
         else{
           recurseEl(obj,obj);
         }
-    	}
+      }
     catch (error) {
-      //console.error(error);
+      console.error(error);
     }
   });
 };
 
 var intervalId = window.setInterval(function(){
-	updateHTML();
+  updateHTML();
 }, 2000);
-
 
